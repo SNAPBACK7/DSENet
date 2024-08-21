72350983139fe488a305d3c1878b52f3a76a1b50
SNAPBACK7 <130534993+SNAPBACK7@users.noreply.github.com>
2024-08-21 12:29:54 +0800

    Delete dense_heads directory

--- dense_heads/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-
-from .h2rbox_head import H2RBoxHead
-from .h2rbox_v2_head import H2RBoxV2Head
-from .h2rbox_v2_nas_head import H2RBoxNASV2Head
-from .h2rbox_v2_auto_head import H2RBoxAutoV2Head
-__all__ = [ 'H2RBoxHead', 'H2RBoxV2Head','H2RBoxNASV2Head',
-    'H2RBoxAutoV2Head'
-]

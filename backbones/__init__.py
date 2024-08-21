c167514416428234a5ca917a363f4a0a8a786c31
SNAPBACK7 <130534993+SNAPBACK7@users.noreply.github.com>
2024-08-21 12:29:40 +0800

    Delete backbones directory

--- backbones/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .re_resnet import ReResNet
-from .lsknet import LSKNet
-from .trident_resnet import TridentResNet
-__all__ = ['ReResNet', 'LSKNet', 'TridentResNet']
